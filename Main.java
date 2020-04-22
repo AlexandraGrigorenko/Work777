eb5ede8f394f0d5e9d0dcab995c4cb48a3d4a4e8
AlexandraGrigorenko <60101421+AlexandraGrigorenko@users.noreply.github.com>
2020-04-22 14:19:42 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Class c = MyClass.class;
+
+        Object testObj = c.newInstance();
+
+        Method[] methods = c.getDeclaredMethods();
+
+        ArrayList<Method> al = new ArrayList<>();
+
+        Method beforeMethod = null;
+
+        Method afterMethod = null;
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 0; j < methods.length; j++) {
+
+                if (methods[j].getAnnotation(Test.class) != null) {
+
+                    if (methods[j].getAnnotation(Test.class).priority() == j) {
+                        al.add(methods[j]);
+                    }
+
+                }
+            }
+        }
+        if (beforeMethod != null) beforeMethod.invoke(testObj, null);
+
+        for (Method o : al) o.invoke(testObj, null);
+
+        if (afterMethod != null) afterMethod.invoke(testObj, null);
+    }
+}
